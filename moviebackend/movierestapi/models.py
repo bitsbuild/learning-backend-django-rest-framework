a5057af8a5a4ba4c841b3f9d352953ac9274ef54
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-10 16:55:09 +0530

    Created Movie Model

--- moviebackend/movierestapi/models.py
+++ moviebackend/movierestapi/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    isReleased = models.BooleanField(default=True)
+    reviews = models.JSONField(default=list)
+    cast = models.JSONField(default=list)
