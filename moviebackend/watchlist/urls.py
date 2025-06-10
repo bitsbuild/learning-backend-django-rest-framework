6702c2883885a6cec769f36644059322f4f363bb
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-10 09:33:19 +0530

    Recent Previous Commits Were A Django Refresher, Actual Learning Project Starts Now

--- moviebackend/watchlist/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from watchlist.views import get_movie_details,list_movie
-
-urlpatterns = [
-    path('list/',list_movie),
-    path('details/<int:pk>',get_movie_details)
-]
