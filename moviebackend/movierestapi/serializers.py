ee6644030bb11694537c13aabc27e0fca713ce9d
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-15 14:14:44 +0530

    Validation Done Using Validators

--- moviebackend/movierestapi/serializers.py
+++ moviebackend/movierestapi/serializers.py
@@ -1,28 +1,22 @@
 from rest_framework import serializers
 from movierestapi.models import Movie
+def name_length(value):
+    if len(value)>50:
+        raise serializers.ValidationError("Name Too Long! Restrict To 50 Characters")
+    else:
+        return value
+def desc_length(value):
+    if len(value)>250:
+        raise serializers.ValidationError("Description Too Long! Restrict To 250 Characters")
+    else:
+        return value
 class MovieSerializer(serializers.Serializer):
     id = serializers.UUIDField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
+    description = serializers.CharField(validators=[desc_length])
     isReleased = serializers.BooleanField()
     reviews = serializers.JSONField()
     cast = serializers.JSONField()
-    # FEILD LEVEL VALIDATION BELOW HERE
-    def validate_name(self,value):
-        if type(value) != str:
-            raise serializers.ValidationError("String Movie Names Only")
-        elif len(value)>50:
-            raise serializers.ValidationError("Name Is Too Long, Restrict To 50 Characters")
-        else:
-            return value
-    def validate_description(self,value):
-        if type(value) != str:
-            raise serializers.ValidationError("String Description Only")
-        elif len(value) > 250:
-            raise serializers.ValidationError("Description Is Too Long, Restrict To 250 Characters")
-        else:
-            return value
-    # FEILD LEVEL VALIDATION ABOVE HERE
     # BELOW FUNCTION: CREATE IS CALLED ON POST AND UPDATE IS CALLED ON PUT BY THE SERIALIZER
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
