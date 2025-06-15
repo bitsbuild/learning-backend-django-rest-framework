b186febacc5443d205b8dd5b71abcb7e7378ddb5
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-15 11:47:50 +0530

    Feild Level Validation Added

--- moviebackend/movierestapi/serializers.py
+++ moviebackend/movierestapi/serializers.py
@@ -8,6 +8,23 @@ class MovieSerializer(serializers.Serializer):
     isReleased = serializers.BooleanField()
     reviews = serializers.JSONField()
     cast = serializers.JSONField()
+    # FEILD LEVEL VALIDATION BELOW HERE
+    def validate_name(self,value):
+        if type(value) != str:
+            raise serializers.ValidationError("String Movie Names Only")
+        elif len(value)>50:
+            raise serializers.ValidationError("Name Is Too Long, Restrict To 50 Characters")
+        else:
+            return value
+    def validate_description(self,value):
+        if type(value) != str:
+            raise serializers.ValidationError("String Description Only")
+        elif len(value) > 250:
+            raise serializers.ValidationError("Description Is Too Long, Restrict To 250 Characters")
+        else:
+            return value
+    # FEILD LEVEL VALIDATION ABOVE HERE
+    # BELOW FUNCTION: CREATE IS CALLED ON POST AND UPDATE IS CALLED ON PUT BY THE SERIALIZER
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
     def update(self, instance, validated_data):
@@ -18,3 +35,4 @@ class MovieSerializer(serializers.Serializer):
         instance.cast = validated_data.get('cast',instance.cast)
         instance.save()
         return instance
+    
