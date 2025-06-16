3f4ff9c2d6da8dd27e68835187fe3e8142b85044
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-16 17:19:02 +0530

    Fresh-4: Models Without Relationships Defined

--- contentreviews/app/models.py
+++ contentreviews/app/models.py
@@ -7,6 +7,7 @@ class StreamingPlatform(models.Model):
     platform_name = models.CharField(max_length=70)
     platform_about = models.CharField(max_length=350)
     platform_url = models.URLField()
+    
 class ContentDetails(models.Model):
     content_id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     content_name = models.CharField(max_length=70)
@@ -14,6 +15,7 @@ class ContentDetails(models.Model):
     content_released = models.BooleanField(default=True)
     content_created = models.DateTimeField(auto_now_add=True,editable=False)
     content_updated = models.DateTimeField(default=timezone.now,editable=True)
+
 class ContentReviews(models.Model):
     review_id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     review_name = models.CharField(max_length=70)
@@ -21,6 +23,7 @@ class ContentReviews(models.Model):
     review_stars = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
     review_created = models.DateTimeField(auto_now_add=True,editable=False)
     review_updated = models.DateTimeField(default=timezone.now,editable=True)
+
 class Artists(models.Model):
     artist_id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     artist_name = models.CharField(max_length=70)
