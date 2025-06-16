b081b0d3d6c486fd7374e2c574c07782ba7a0812
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-16 22:21:57 +0530

    Fresh-15: Streaming Platform And Artists GET All And POST New Done

--- contentreviews/app/urls.py
+++ contentreviews/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app.views import ContentAV,ArtistAV
+from app.views import ContentAV,ArtistAV,StreamingPlatformAV
 urlpatterns = [
     path('content-list/',ContentAV.as_view()),
     path('artist-list/',ArtistAV.as_view()),
+    path('platforms-list/',StreamingPlatformAV.as_view()),
 ]
--- contentreviews/app/views.py
+++ contentreviews/app/views.py
@@ -33,10 +33,17 @@ class ArtistAV(APIView):
             return Response(serialized_artist.errors,status=status.HTTP_400_BAD_REQUEST)
 class StreamingPlatformAV(APIView):
     def get(self,request):
-        pass
+        platforms = StreamingPlatform.objects.all()
+        serialized_platforms = StreamingPlatformSerializer(platforms,many=True)
+        return Response(serialized_platforms.data,status=status.HTTP_200_OK)
     @swagger_auto_schema(request_body=StreamingPlatformSerializer)
     def post(self,request):
-        pass
+        new_platform = StreamingPlatformSerializer(data=request.data)
+        if new_platform.is_valid():
+            new_platform.save()
+            return Response(new_platform.data,status=status.HTTP_200_OK)
+        else:
+            return Response(new_platform.errors,status=status.HTTP_400_BAD_REQUEST)
 class ContentReviewAV(APIView):
     def get(self,request):
         pass
