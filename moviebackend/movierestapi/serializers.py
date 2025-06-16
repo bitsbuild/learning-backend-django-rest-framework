5d5d64c5403d33dc7341de2940dfb1ede6537608
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-16 12:20:49 +0530

    Formatting

--- moviebackend/movierestapi/serializers.py
+++ moviebackend/movierestapi/serializers.py
@@ -2,12 +2,9 @@ from rest_framework import serializers
 from movierestapi.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
-    len_names = serializers.SerializerMethodField(
-
-    )
+    len_names = serializers.SerializerMethodField()
     class Meta:
         model = Movie
         fields = '__all__'
-    
     def get_len_names(self,object):
         return len(object.name)
