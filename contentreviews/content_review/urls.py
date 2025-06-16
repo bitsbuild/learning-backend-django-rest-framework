7cf5a057ef19bfaf1e0847ab78aa180023947318
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-16 18:00:29 +0530

    Swagger UI Added, When Views Will Be Added Then Swagger Configuration Will Be Able To Complete

--- contentreviews/content_review/urls.py
+++ contentreviews/content_review/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Content Review Backend',
+        default_version='v1',
+        description='Review Any Piece Of Content With The Help Of Wide Range Of Our APIs'
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
 urlpatterns = [
+    path('',schema_view.with_ui('swagger'),name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('rest-apis/','app.urls'),
 ]
