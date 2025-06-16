413ac8be4db63c6e8ace0badd86985fd49060605
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-16 18:01:23 +0530

    Fresh-8: Swagger UI Partial Set-Ip

--- contentreviews/content_review/urls.py
+++ contentreviews/content_review/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+
 schema_view = get_schema_view(
     openapi.Info(
         title='Content Review Backend',
@@ -12,6 +13,7 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(permissions.AllowAny,)
 )
+
 urlpatterns = [
     path('',schema_view.with_ui('swagger'),name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
