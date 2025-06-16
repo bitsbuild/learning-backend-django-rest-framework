d91f5c94d78d5f41b2632280aceafaf4e982d903
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-16 18:07:40 +0530

    Fresh-9: Model Labels Added

--- contentreviews/app/models.py
+++ contentreviews/app/models.py
@@ -7,7 +7,8 @@ class StreamingPlatform(models.Model):
     platform_name = models.CharField(max_length=70)
     platform_about = models.CharField(max_length=350)
     platform_url = models.URLField()
-    
+    def __str__(self):
+        return self.platform_name
 class ContentDetails(models.Model):
     content_id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     content_name = models.CharField(max_length=70)
@@ -15,7 +16,8 @@ class ContentDetails(models.Model):
     content_released = models.BooleanField(default=True)
     content_created = models.DateTimeField(auto_now_add=True,editable=False)
     content_updated = models.DateTimeField(default=timezone.now,editable=True)
-
+    def __str__(self):
+        return self.content_name
 class ContentReviews(models.Model):
     review_id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     review_name = models.CharField(max_length=70)
@@ -23,8 +25,11 @@ class ContentReviews(models.Model):
     review_stars = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
     review_created = models.DateTimeField(auto_now_add=True,editable=False)
     review_updated = models.DateTimeField(default=timezone.now,editable=True)
-
+    def __str__(self):
+        return self.review_name
 class Artists(models.Model):
     artist_id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     artist_name = models.CharField(max_length=70)
     artist_about = models.CharField(max_length=350)
+    def __str__(self):
+        return self.artist_name
