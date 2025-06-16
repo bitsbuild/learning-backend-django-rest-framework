4ba9f93417d4149a2ca89a54d203c9b46a77eae5
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-16 15:55:27 +0530

    partial cleanup

--- moviebackend/movierestapi/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from movierestapi.views import MoviesAV,MovieDetailAV
-urlpatterns = [
-    path('movies/',MoviesAV.as_view()),
-    path('movie-detail/<uuid:id>',MovieDetailAV.as_view()),
-]
