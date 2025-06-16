0b99452ef99f9bb469f77e2c03225afc1f7849a0
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-16 19:45:51 +0530

    Fresh-11: Migrations Made

--- contentreviews/content_review/urls.py
+++ contentreviews/content_review/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -17,5 +17,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('',schema_view.with_ui('swagger'),name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
-    path('rest-apis/','app.urls'),
+    path('rest-apis/',include('app.urls')),
 ]
