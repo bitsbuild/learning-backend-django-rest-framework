4cf5e95f63987ef669a90635cd15a0fd99f671e1
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-16 19:44:15 +0530

    Fresh-10: Model Created

--- contentreviews/app/models.py
+++ contentreviews/app/models.py
@@ -9,6 +9,12 @@ class StreamingPlatform(models.Model):
     platform_url = models.URLField()
     def __str__(self):
         return self.platform_name
+class Artists(models.Model):
+    artist_id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
+    artist_name = models.CharField(max_length=70)
+    artist_about = models.CharField(max_length=350)
+    def __str__(self):
+        return self.artist_name
 class ContentDetails(models.Model):
     content_id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     content_name = models.CharField(max_length=70)
@@ -16,6 +22,8 @@ class ContentDetails(models.Model):
     content_released = models.BooleanField(default=True)
     content_created = models.DateTimeField(auto_now_add=True,editable=False)
     content_updated = models.DateTimeField(default=timezone.now,editable=True)
+    content_platform = models.ForeignKey(StreamingPlatform,on_delete=models.CASCADE,related_name="content")
+    artists = models.ManyToManyField(Artists,related_name="content",blank=True)
     def __str__(self):
         return self.content_name
 class ContentReviews(models.Model):
@@ -25,11 +33,6 @@ class ContentReviews(models.Model):
     review_stars = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
     review_created = models.DateTimeField(auto_now_add=True,editable=False)
     review_updated = models.DateTimeField(default=timezone.now,editable=True)
+    review_movie = models.ForeignKey(ContentDetails,on_delete=models.CASCADE,related_name="reviews")
     def __str__(self):
         return self.review_name
-class Artists(models.Model):
-    artist_id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
-    artist_name = models.CharField(max_length=70)
-    artist_about = models.CharField(max_length=350)
-    def __str__(self):
-        return self.artist_name
