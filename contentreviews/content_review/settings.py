decd588d429e5c84cbeb351a5c6b634613341202
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-17 21:56:40 +0530

    Fresh-41: Django Filter Added

--- contentreviews/content_review/settings.py
+++ contentreviews/content_review/settings.py
@@ -65,3 +65,6 @@ USE_TZ = True
 USE_I18N = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
