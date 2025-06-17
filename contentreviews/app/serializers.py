75bd02f4fefbd4485ba48a22c7249c38c843e506
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-17 07:45:23 +0530

    Fresh-22: Serializer Relations Added

--- contentreviews/app/serializers.py
+++ contentreviews/app/serializers.py
@@ -1,22 +1,25 @@
 from app.models import ContentDetails,StreamingPlatform,Artists,ContentReviews
 from rest_framework import serializers
+class ContentReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContentReviews
+        feilds = '__all__'
+        exclude = []
 class ContentSerializer(serializers.ModelSerializer):
+    reviews = ContentReviewSerializer(many=True,read_only=True)
     class Meta:
         model = ContentDetails
         feilds = '__all__'
         exclude = []
 class StreamingPlatformSerializer(serializers.ModelSerializer):
+    content = ContentSerializer(many=True,read_only=True)
     class Meta:
         model = StreamingPlatform
         feilds = '__all__'
         exclude = []
 class ArtistsSerializer(serializers.ModelSerializer):
+    content = ContentSerializer(many=True,read_only=True)
     class Meta:
         model = Artists
-        feilds = '__all__'
-        exclude = []
-class ContentReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContentReviews
         feilds = '__all__'
         exclude = []
