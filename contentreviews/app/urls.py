61c5ca1bd582396348828a92eeb80caa8b6745cb
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-17 22:03:23 +0530

    Fresh-43: Endpoint Functionality Wise Development Complete

--- contentreviews/app/urls.py
+++ contentreviews/app/urls.py
@@ -11,13 +11,22 @@ from app.views import (ContentListCreateAV,
                        ContentPerArtistAV,)
 urlpatterns = [
     path('content/',ContentListCreateAV.as_view()),
+
     path('content/<uuid:pk>/',ContentDetailAV.as_view()),
+
     path('artist/',ArtistListCreateAV.as_view()),
+
     path('artist/<uuid:pk>/',ArtistDetailAV.as_view()),
+
     path('platform/',StreamingPlatformListCreateAV.as_view()),
+
     path('platform/<uuid:pk>/',StreamingPlatformDetailAV.as_view()),
+
     path('review/',ReviewsListCreateAV.as_view()),
+
     path('review/<uuid:pk>/',ReviewsDetailAV.as_view()),
+
     path('platform/<uuid:pk>/contents/',ContentPerPlatformAV.as_view()),
+    
     path('artist/<uuid:pk>/contents/',ContentPerArtistAV.as_view()),
 ]
