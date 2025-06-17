eb92da149cc48b5b8c496a1b2536177c5b381bda
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-17 20:43:12 +0530

    Fresh-38: Minor Reference Fix With Runtime Changes

--- contentreviews/app/urls.py
+++ contentreviews/app/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from app.views import ContentListCreateAV,ArtistListCreateAV,StreamingPlatformListCreateAV,ReviewsListCreateAV,ContentDetailAV,ArtistDetailAV,StreamingPlatformDetailAV,ReviewsDetailAV
 urlpatterns = [
     path('content-list/',ContentListCreateAV.as_view()),
-    path('content-update-delete/<uuid:id>',ContentDetailAV.as_view()),
+    path('content-update-delete/<uuid:pk>',ContentDetailAV.as_view()),
     path('artist-list/',ArtistListCreateAV.as_view()),
-    path('artist-update-delete/<uuid:id>',ArtistDetailAV.as_view()),
+    path('artist-update-delete/<uuid:pk>',ArtistDetailAV.as_view()),
     path('platforms-list/',StreamingPlatformListCreateAV.as_view()),
-    path('platform-update-delete/<uuid:id>',StreamingPlatformDetailAV.as_view()),
+    path('platform-update-delete/<uuid:pk>',StreamingPlatformDetailAV.as_view()),
     path('content-review/',ReviewsListCreateAV.as_view()),
-    path('review-update-delete/<uuid:id>',ReviewsDetailAV.as_view()),
+    path('review-update-delete/<uuid:pk>',ReviewsDetailAV.as_view()),
 ]
