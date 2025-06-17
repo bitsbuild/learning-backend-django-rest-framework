6f5ba100c1b6931045c21d9873008d0cf671adc9
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-17 09:58:35 +0530

    Fresh-25: Previously Model Removed Brought In Again To Complete The Model

--- contentreviews/app/models.py
+++ contentreviews/app/models.py
@@ -7,12 +7,16 @@ class StreamingPlatform(models.Model):
     platform_name = models.CharField(max_length=70)
     platform_about = models.CharField(max_length=350)
     platform_url = models.URLField()
+    platform_created = models.DateTimeField(auto_now_add=True,editable=False)
+    platform_updated = models.DateTimeField(auto_now=True,editable=False)
     def __str__(self):
         return self.platform_name
 class Artists(models.Model):
     artist_id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
     artist_name = models.CharField(max_length=70)
     artist_about = models.CharField(max_length=350)
+    artist_created = models.DateTimeField(auto_now_add=True,editable=False)
+    artist_updated = models.DateTimeField(auto_now=True,editable=False)
     def __str__(self):
         return self.artist_name
 class ContentDetails(models.Model):
@@ -21,6 +25,7 @@ class ContentDetails(models.Model):
     content_description = models.CharField(max_length=350)
     content_released = models.BooleanField(default=True)
     content_created = models.DateTimeField(auto_now_add=True,editable=False)
+    content_updated = models.DateTimeField(auto_now=True,editable=False)
     content_platform = models.ForeignKey(StreamingPlatform,on_delete=models.CASCADE,related_name="content")
     artists = models.ManyToManyField(Artists,related_name="content",blank=True)
     def __str__(self):
@@ -31,6 +36,7 @@ class ContentReviews(models.Model):
     review_body = models.CharField(max_length=350)
     review_stars = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
     review_created = models.DateTimeField(auto_now_add=True,editable=False)
+    review_updated = models.DateTimeField(auto_now=True,editable=False)
     review_movie = models.ForeignKey(ContentDetails,on_delete=models.CASCADE,related_name="reviews")
     def __str__(self):
         return self.review_name
