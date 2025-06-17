1ea63e69826c49f4dfc76a4073422ddb3b5d69ac
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-17 21:17:45 +0530

    Fresh-39: Naming URLs As Per REST Convention

--- contentreviews/app/urls.py
+++ contentreviews/app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from app.views import ContentListCreateAV,ArtistListCreateAV,StreamingPlatformListCreateAV,ReviewsListCreateAV,ContentDetailAV,ArtistDetailAV,StreamingPlatformDetailAV,ReviewsDetailAV
 urlpatterns = [
-    path('content-list/',ContentListCreateAV.as_view()),
-    path('content-update-delete/<uuid:pk>',ContentDetailAV.as_view()),
-    path('artist-list/',ArtistListCreateAV.as_view()),
-    path('artist-update-delete/<uuid:pk>',ArtistDetailAV.as_view()),
-    path('platforms-list/',StreamingPlatformListCreateAV.as_view()),
-    path('platform-update-delete/<uuid:pk>',StreamingPlatformDetailAV.as_view()),
-    path('content-review/',ReviewsListCreateAV.as_view()),
-    path('review-update-delete/<uuid:pk>',ReviewsDetailAV.as_view()),
+    path('content/',ContentListCreateAV.as_view()),
+    path('content/<uuid:pk>/',ContentDetailAV.as_view()),
+    path('artist/',ArtistListCreateAV.as_view()),
+    path('artist/<uuid:pk>/',ArtistDetailAV.as_view()),
+    path('platforms/',StreamingPlatformListCreateAV.as_view()),
+    path('platform/<uuid:pk>/',StreamingPlatformDetailAV.as_view()),
+    path('review/',ReviewsListCreateAV.as_view()),
+    path('review/<uuid:pk>/',ReviewsDetailAV.as_view()),
 ]
