6d811566eb5a772e9270de219e5b273feed42093
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-18 12:56:11 +0530

    Fresh-46: All Endpoints Completed Functionality Wise With Minimum Lines Of Code Using ModelViewSets, Routers, ModelSerializers

--- contentreviews/app/views.py
+++ contentreviews/app/views.py
@@ -1,9 +1,12 @@
-from rest_framework import viewsets,generics
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from app.models import ContentDetails, ContentReviews, Artists, StreamingPlatform
 from app.serializers import ContentSerializer,ContentReviewSerializer,ArtistsSerializer,StreamingPlatformSerializer
 class ContentViewSet(viewsets.ModelViewSet):
     queryset = ContentDetails.objects.all()
     serializer_class = ContentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['artists','content_platform']
 class ArtistViewSet(viewsets.ModelViewSet):
     queryset = Artists.objects.all()
     serializer_class = ArtistsSerializer
