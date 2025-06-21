fc51ef671bea954bf08d90c29e3f59a02aa0d9ac
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-21 15:05:08 +0530

    Added Incorrect Placeholder For Logout Endpoint

--- contentreviews/user/urls.py
+++ contentreviews/user/urls.py
@@ -4,10 +4,8 @@ from user.views import UserRegistrationVS
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'register',UserRegistrationVS,basename='register')
-def logout():
-    return 1
 urlpatterns = [
     path('',include(router.urls)),
     path('login/',obtain_auth_token,name='login'),
-    path('logout/',logout,name='logout'),
+    path('logout/',obtain_auth_token,name='logout'),
 ]
