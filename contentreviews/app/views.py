512889da10b73e18a233e5acce87af086fa7191b
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-21 11:12:13 +0530

    Authentication Made Mandatory For All Endpoints

--- contentreviews/app/views.py
+++ contentreviews/app/views.py
@@ -1,25 +1,25 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from app.models import ContentDetails, ContentReviews, Artists, StreamingPlatform
 from app.serializers import ContentSerializer,ContentReviewSerializer,ArtistsSerializer,StreamingPlatformSerializer
-from app.permissions import AdminOrReadOnly,ReviewPermissions
+from app.permissions import AdminOrReadOnly,ReviewPermissions 
 class ContentViewSet(viewsets.ModelViewSet):
     queryset = ContentDetails.objects.all()
     serializer_class = ContentSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['artists','content_platform']
-    permission_classes=[AdminOrReadOnly]
+    permission_classes=[permissions.IsAuthenticated,AdminOrReadOnly]
 class ArtistViewSet(viewsets.ModelViewSet):
     queryset = Artists.objects.all()
     serializer_class = ArtistsSerializer
-    permission_classes=[AdminOrReadOnly]
+    permission_classes=[permissions.IsAuthenticated,AdminOrReadOnly]
 class PlatformViewSet(viewsets.ModelViewSet):
     queryset = StreamingPlatform.objects.all()
     serializer_class = StreamingPlatformSerializer
-    permission_classes=[AdminOrReadOnly]
+    permission_classes=[permissions.IsAuthenticated,AdminOrReadOnly]
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = ContentReviews.objects.all()
     serializer_class = ContentReviewSerializer
-    permission_classes = [ReviewPermissions]
+    permission_classes = [permissions.IsAuthenticated,ReviewPermissions]
     def perform_create(self, serializer):
         serializer.save(review_user=self.request.user)
