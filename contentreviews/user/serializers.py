7f9abd10194f804e103f2ea2fd0bcd8cfb61c32a
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-21 13:49:17 +0530

    Confirm Password Feild Added With Input Style And Write Only Access

--- contentreviews/user/serializers.py
+++ contentreviews/user/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer,CharField
 class UserSerializer(ModelSerializer):
+    confirm_password = CharField(write_only=True,style={'input_type':'password'})
     class Meta:
         model = User
         fields = ['username','email','password','confirm_password']
