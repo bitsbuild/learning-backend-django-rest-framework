d3127d8a2505abf8b87065c0742e822475205f02
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-21 16:51:49 +0530

    Removing Unneccessary Code, Token Authentication Completed

--- contentreviews/user/serializers.py
+++ contentreviews/user/serializers.py
@@ -1,14 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer,CharField,ValidationError
 class UserSerializer(ModelSerializer):
-    confirm_password = CharField(write_only=True,style={'input_type':'password'})
+    confirm_password = CharField(write_only=True,style={'input_type':'password'},max_length=128)
     class Meta:
         model = User
         fields = ['username','email','password','confirm_password']
-        extra_kwargs = {
-            'password':{'write_only':True},
-            'style':{'input_type':'password'},
-        }
     def save(self):
         if self.validated_data['password'] == self.validated_data['confirm_password']:
             if User.objects.filter(email=self.validated_data['email']).exists():
