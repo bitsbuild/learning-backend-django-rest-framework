8c8eccbbb2a60e5cc0bb81dc581243339f403732
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-21 14:09:21 +0530

    View Created For User Registration

--- contentreviews/user/views.py
+++ contentreviews/user/views.py
@@ -1,1 +1,5 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+class UserRegistrationAV(CreateAPIView):
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
