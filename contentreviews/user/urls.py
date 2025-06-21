c419b4751a371b90dc8ea70b290befebfcd52665
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-21 13:15:22 +0530

    Placeholders Created For Setting Up URLs Structure

--- contentreviews/user/urls.py
+++ contentreviews/user/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+def logout():
+    return 1
+def register():
+    return 0
 urlpatterns = [
-    path('login/',obtain_auth_token,name='login')
+    path('register/',register,name='register'),
+    path('login/',obtain_auth_token,name='login'),
+    path('logout/',logout,name='logout'),
 ]
