62646cf90f647325d0938dc38b2fe24685839b78
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-22 18:27:36 +0530

    Pagination Configured And Tested And Fixed

--- contentreviews/app/pagination.py
+++ contentreviews/app/pagination.py
@@ -4,6 +4,6 @@ class ContentsPagination(PageNumberPagination):
     # page_query_param = 'p' instead of page= we do p= in url to access page
     page_size_query_param = 'size'
     max_page_size = 30
-    last_page_strings = 'end'
+    last_page_strings = ['end']
     invalid_page_message = 'Invalid Page Selected'
     display_page_controls = True
