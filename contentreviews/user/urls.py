f3ee8ff03464a55b9cc714cd8121cf4f1fefd73d
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-22 11:23:58 +0530

    User Authentication Endpoints Written, Testing And Fixing If Any For Auth Up Next

--- contentreviews/user/urls.py
+++ contentreviews/user/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from user.views import create_account,delete_account
 urlpatterns = [
-    path('get-token/',obtain_auth_token,name='get-token')
+    path('create/',create_account,name='create'),
+    path('token/',obtain_auth_token,name='token'),
+    path('delete/',delete_account,name='delete')
 ]
--- contentreviews/user/views.py
+++ contentreviews/user/views.py
@@ -1,1 +1,48 @@
-from django.shortcuts import render
+from user.serializers import UserSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token,User
+@api_view(['POST'])
+def create_account(request):
+    username = request.data['username']
+    password = request.data['password']
+    confirm_password = request.data['confirm_password']
+    email = request.data['email']
+    if confirm_password == password:
+        user = {
+            "username":username,
+            "password":password,
+            "email":email
+        }
+        serializer = UserSerializer(data=user)
+        if serializer.is_valid():
+            serializer.save()
+            user_token_search = User.objects.get(username=username)
+            token,collect = Token.objects.get_or_create(user=user_token_search)
+            return Response({
+                "status":"Account Created Successfully",
+                "token":str(token.key)
+            },status=status.HTTP_200_OK)
+        else:
+            return Response({
+                "status":"Account Creation Failed",
+                "error":str(serializer.errors)
+            },status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({
+            "status":"Account Creation Failed",
+            "error":str(serializer.errors)
+        },status=status.HTTP_400_BAD_REQUEST)
+@api_view(['POST'])
+def delete_account(request):
+    try:    
+        request.user.delete()
+        return Response({
+            "status":"Account Deletion Successful"
+        },status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({
+            "status":"Account Deletion Failed",
+            "error":str(e)
+        },status=status.HTTP_400_BAD_REQUEST)
