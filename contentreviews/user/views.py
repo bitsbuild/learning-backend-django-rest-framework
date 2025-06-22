758f5649ed9289c7ff789a217e402ab615a25a2f
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-22 11:27:13 +0530

    User Imported From Wrong Library

--- contentreviews/user/views.py
+++ contentreviews/user/views.py
@@ -2,7 +2,8 @@ from user.serializers import UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token,User
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 @api_view(['POST'])
 def create_account(request):
     username = request.data['username']
