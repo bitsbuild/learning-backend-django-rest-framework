3fdd1e6bfbaeb87c00cd2dd99e5b37dc749a4ea0
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-22 18:08:03 +0530

    Pagination Configured

--- contentreviews/app/pagination.py
+++ contentreviews/app/pagination.py
@@ -1,3 +1,9 @@
 from rest_framework.pagination import PageNumberPagination
 class ContentsPagination(PageNumberPagination):
-    pass
+    page_size = 3
+    # page_query_param = 'p' instead of page= we do p= in url to access page
+    page_size_query_param = 'size'
+    max_page_size = 30
+    last_page_strings = 'end'
+    invalid_page_message = 'Invalid Page Selected'
+    display_page_controls = True
