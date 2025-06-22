c986ee7fb82e28dd77fd79f13d2a8b732002adf0
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-22 14:58:52 +0530

    Global Throttling Commented Out To Apply Specific Throttling

--- contentreviews/content_review/settings.py
+++ contentreviews/content_review/settings.py
@@ -71,10 +71,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.AnonRateThrottle',
+    #     'rest_framework.throttling.UserRateThrottle'
+    # ],
     'DEFAULT_THROTTLE_RATES': {
         'anon':'50/hour',
         'user':'100/hour'
