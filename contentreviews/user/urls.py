97a03427fb63c5fb56196e42d6fec2b9c69233a8
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-22 21:02:43 +0530

    Commented Out Token Auhtentication And Partial Set Up Done For JWT Authentication

--- contentreviews/user/urls.py
+++ contentreviews/user/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-from user.views import create_account,delete_account
+# from rest_framework.authtoken.views import obtain_auth_token
+# from user.views import create_account,delete_account
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 urlpatterns = [
-    path('create/',create_account,name='create'),
-    path('token/',obtain_auth_token,name='token'),
-    path('delete/',delete_account,name='delete')
+    # path('create/',create_account,name='create'),
+    # path('token/',obtain_auth_token,name='token'),
+    # path('delete/',delete_account,name='delete')
+    path('api/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('api/token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
+    
 ]
