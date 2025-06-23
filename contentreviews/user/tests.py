c25f3ebc6cd71db56b1dfcdb0e7ee3f841bb2fb4
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-23 15:24:24 +0530

    Minor Test File Refactoring

--- contentreviews/user/tests.py
+++ contentreviews/user/tests.py
@@ -1,10 +1,10 @@
+# The Testing File Name Should Not Be Changed
+# The Endpoints Testing Function Should Begin With "test_x" Strictly As Thier Function Name
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
 from django.contrib.auth.models import User
 class UserTests(APITestCase):
-    # The Testing File Name Should Not Be Changed
-    # The Endpoints Testing Function Should Begin With "test_x" Strictly As Thier Function Name
     def test_create_user_success(self):
         data = {
             "username":"testcase",
