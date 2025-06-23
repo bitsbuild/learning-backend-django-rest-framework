d11fece98a898a95a647a2943f86f5649f94a45b
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-23 15:43:07 +0530

    Account Creation, Account Deletion, Token Obtain Endpoints Success Tests Tested

--- contentreviews/user/tests.py
+++ contentreviews/user/tests.py
@@ -4,6 +4,7 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 class UserTests(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="jashupadhyay",password="12345678")
@@ -24,4 +25,7 @@ class UserTests(APITestCase):
         response = self.client.post(reverse('token'),data)
         self.assertEqual(response.status_code,status.HTTP_200_OK)
     def test_delete_user(self):
-        pass
+        self.token = Token.objects.get(user__username='jashupadhyay')
+        self.client.credentials(HTTP_AUTHORIZATION='Token '+self.token.key)
+        respone = self.client.post(reverse('delete'))
+        self.assertEqual(respone.status_code,status.HTTP_200_OK)
