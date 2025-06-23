1d1f607ef5be99e986d93592fb002b64d202c88e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-23 15:23:44 +0530

    Account Creation Test Written And Executed Tests Passed By The Endpoint

--- contentreviews/user/tests.py
+++ contentreviews/user/tests.py
@@ -1,10 +1,18 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
+from django.contrib.auth.models import User
 class UserTests(APITestCase):
     # The Testing File Name Should Not Be Changed
     # The Endpoints Testing Function Should Begin With "test_x" Strictly As Thier Function Name
-    def test_create_user(self):
-        pass
+    def test_create_user_success(self):
+        data = {
+            "username":"testcase",
+            "password":"jash_password",
+            "confirm_password":"jash_password",
+            "email":"testcase@example.come"
+        }
+        response = self.client.post(reverse('create'),data)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
     def test_delete_user(self):
         pass
