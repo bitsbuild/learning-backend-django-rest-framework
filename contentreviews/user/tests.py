9bc326878f61a68a51f2afceb7d8378123438ea5
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-23 15:02:39 +0530

    Basic Test File Structure Defined For User App

---
+++ contentreviews/user/tests.py
@@ -0,0 +1,10 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+class UserTests(APITestCase):
+    # The Testing File Name Should Not Be Changed
+    # The Endpoints Testing Function Should Begin With "test_x" Strictly As Thier Function Name
+    def test_create_user(self):
+        pass
+    def test_delete_user(self):
+        pass
