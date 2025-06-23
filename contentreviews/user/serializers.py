bfc6b30059771a44d6376b28bff83333a567dcb1
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-23 17:03:12 +0530

    Serializer File Fixed For User App

--- contentreviews/user/serializers.py
+++ contentreviews/user/serializers.py
@@ -1,10 +1,25 @@
 from django.contrib.auth.models import User
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer,CharField,ValidationError
 class UserSerializer(ModelSerializer):
+    confirm_password = CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['username','email','password']
-    def save(self):
-        user = User(username=self.validated_data['username'],email=self.validated_data['email'])
-        user.set_password(self.validated_data['password'])
+        fields = ['username','email','password','confirm_password']
+        extra_kwargs = {
+            "password":{
+                'write_only':True
+            },
+        }
+    def validate(self,data):
+        if data['password'] == data['confirm_password']:
+            return data
+        else:
+            raise ValidationError({
+                "error":"Passwords Don't Match"
+            })
+    def create(self,validated_data):
+        validated_data.pop('confirm_password')
+        user = User(username=validated_data['username'],email=validated_data['email'])
+        user.set_password(validated_data['password'])
         user.save()
+        return user
